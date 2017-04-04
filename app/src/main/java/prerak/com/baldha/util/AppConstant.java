8c4583c28fd0dc1929ad48658388aaf561cce546
Bhadresh Chavada <bhadreshchavada@gmail.com>
2017-04-05 00:46:07 +0530

    URL

--- app/src/main/java/prerak/com/baldha/util/AppConstant.java
+++ app/src/main/java/prerak/com/baldha/util/AppConstant.java
@@ -56,5 +56,19 @@ public static String BASE_URL = "http://yogeshkachhad.16mb.com/";
     public static void hideProgressDialog() {
         progressDialog.dismiss();
     }
+
+//    login	http://yogeshkachhad.16mb.com/webservices/login?username=superuser&password=super123
+//    forgot password	http://localhost/adminpanel/webservices/forgot_password?email=kachhadyogesh050@gmail.com
+//    category	http://yogeshkachhad.16mb.com/webservices/getCategory
+//    product by catID	http://yogeshkachhad.16mb.com/webservices/getProduct?catID=1
+//    country	http://yogeshkachhad.16mb.com/webservices/getCountry
+//    state	http://yogeshkachhad.16mb.com/webservices/getState
+//    district	http://yogeshkachhad.16mb.com/webservices/getDistrict
+//    city	http://yogeshkachhad.16mb.com/webservices/getCity
+//    insertshop	http://yogeshkachhad.16mb.com/webservices/insertShop?shopName=xyz&ownerName=xyz&address=qwe&cityID=1&areaID=1&distributorID=1&contactNO=9909556304&image=&lat=31.00&long=51.00
+//    getshop	http://yogeshkachhad.16mb.coim/webservices/getShop?userID
+//    inserOrder	 http://yogeshkachhad.16mb.com/webservices/insertOrder?userID=1&shopID=1&productID=1&cityID=1&categoryID=1&batterylvl=10&lat=31.00&long=33.56
+//
+//    getarea new	http://yogeshkachhad.16mb.com/webservices/getArea?userID=1
 }
 
